93734d237806afc30f3cd456bfd6787bac889dcf
JoduT <Sashakalachev73@gmail.com>
2024-05-10 16:30:06 +0300

    app V2

--- app/src/main/java/com/example/projectwithulmas/MainActivity.kt
+++ app/src/main/java/com/example/projectwithulmas/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.projectwithulmas
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectwithulmas.databinding.ActivityMainBinding
 import kotlin.random.Random
@@ -11,24 +10,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
         var randomNumber = Random.nextInt(-10,11)
         var userNumber : Int = 0
-        setContentView(binding.root)
+        var historyText : String = ""
         binding.stebButton.addOnChangeListener { slider, fl, b ->
             userNumber = fl.toInt()
         }
         binding.checkButton.setOnClickListener{
             if(userNumber == randomNumber){
-               binding.textResult.text = "Верно"
+               binding.resultText.text = "Верно"
+                historyText += " Верно"
             }
             else{
                 if(userNumber < randomNumber){
-                    binding.textResult.text = "Больше"
+                    binding.resultText.text = "Больше"
                 }
                 else{
-                    binding.textResult.text = "Меньше"
+                    binding.resultText.text = "Меньше"
                 }
+                historyText += " Неверно"
             }
+            binding.historyText.text = historyText
         }
         binding.restartButton.setOnClickListener{
             randomNumber = Random.nextInt(-10,11)
