992499e1c0381ff6239f8f81ea6024188b7d1c4c
JoduT <Sashakalachev73@gmail.com>
2024-05-10 15:53:54 +0300

    Первая версия приложения

--- app/src/main/java/com/example/projectwithulmas/MainActivity.kt
+++ app/src/main/java/com/example/projectwithulmas/MainActivity.kt
@@ -4,16 +4,34 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projectwithulmas.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        var randomNumber = Random.nextInt(-10,11)
+        var userNumber : Int = 0
         setContentView(binding.root)
         binding.stebButton.addOnChangeListener { slider, fl, b ->
-            binding.textView.text = fl.toInt().toString()
-            Log.d("logging", "Hello")
+            userNumber = fl.toInt()
+        }
+        binding.checkButton.setOnClickListener{
+            if(userNumber == randomNumber){
+               binding.textResult.text = "Верно"
+            }
+            else{
+                if(userNumber < randomNumber){
+                    binding.textResult.text = "Больше"
+                }
+                else{
+                    binding.textResult.text = "Меньше"
+                }
+            }
+        }
+        binding.restartButton.setOnClickListener{
+            randomNumber = Random.nextInt(-10,11)
         }
     }
 }
