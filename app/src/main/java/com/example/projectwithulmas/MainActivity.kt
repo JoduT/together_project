94be60944582220099f3fec282baa2e727c5e1f2
JoduT <Sashakalachev73@gmail.com>
2024-05-10 15:32:48 +0300

    Сделал бета версию проекта

--- app/src/main/java/com/example/projectwithulmas/MainActivity.kt
+++ app/src/main/java/com/example/projectwithulmas/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.projectwithulmas
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.projectwithulmas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.stebButton.addOnChangeListener { slider, fl, b ->
+            binding.textView.text = fl.toInt().toString()
+            Log.d("logging", "Hello")
         }
     }
 }
